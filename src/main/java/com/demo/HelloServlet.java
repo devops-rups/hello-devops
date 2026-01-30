5c2443b8d905c669bd3403c70765f27f565c1d10
devops-rups <rupalikhatri948@gmail.com>
2026-01-30 16:13:07 +0530

    Fix dOCKERFILE: define main branch

--- src/main/java/com/demo/HelloServlet.java
+++ src/main/java/com/demo/HelloServlet.java
@@ -1,19 +1,19 @@
 package com.demo;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/")   // 🔥 YE LINE SABSE IMPORTANT HAI
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         out.println("<h1>Hello DevOps!</h1>");
-        out.close();
     }
 }
